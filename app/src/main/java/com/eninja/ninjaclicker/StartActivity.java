0eeda71ea578c50275d819d72c604e9325ed6b8b
kolya-t <toporkov.kolya96@gmail.com>
2016-09-25 16:59:12 +0300

    Added splashscreen

--- app/src/main/java/com/eninja/ninjaclicker/StartActivity.java
+++ app/src/main/java/com/eninja/ninjaclicker/StartActivity.java
@@ -3,10 +3,30 @@ package com.eninja.ninjaclicker;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class StartActivity extends Activity implements View.OnClickListener {
+
+    private static final int STOPSPLASH = 0;
+    private static final long SPLASHTIME = 5000; // время показа сплэш-картинки
+    private ImageView splashscreen;
+
+    private Handler splashHandler = new Handler() { // создаем новый хэндлер
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STOPSPLASH:
+                    // убираем картинку
+                    splashscreen.setVisibility(View.GONE);
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 
     private Button btnStart;
     private Button btnExit;
@@ -15,6 +35,11 @@ public class StartActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_layout);
+
+        splashscreen = (ImageView) findViewById(R.id.splashscreen);
+        Message msg = new Message();
+        msg.what = STOPSPLASH;
+        splashHandler.sendMessageDelayed(msg, SPLASHTIME);
 
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(this);
